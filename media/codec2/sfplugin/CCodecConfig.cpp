7a9230ba92c796bb24abc7bf913a90d72f8d6fd9
Ram Mohan <ram.mohan@ittiam.com>
2023-12-11 19:41:31 +0530

    CCodec: support region of interest parameters
    Bug: 299191092
    Test: ./ccodec_unit_test \
      --gtest_filter=CCodecConfigTest.SetRegionOfInterestParams
    
    Change-Id: I9654d7815cc8838c781b2655ff7dd7912d40e481

--- media/codec2/sfplugin/CCodecConfig.cpp
+++ media/codec2/sfplugin/CCodecConfig.cpp
@@ -591,6 +591,10 @@ void CCodecConfig::initializeStandardParams() {
             }
             return C2Value();
         }));
+    add(ConfigMapper(KEY_QP_OFFSET_MAP, C2_PARAMKEY_QP_OFFSET_MAP, "value")
+        .limitTo((D::VIDEO | D::IMAGE) & (D::CONFIG | D::PARAM) & D::ENCODER & D::INPUT));
+    add(ConfigMapper(C2_PARAMKEY_QP_OFFSET_RECTS, C2_PARAMKEY_QP_OFFSET_RECTS, "")
+        .limitTo((D::VIDEO | D::IMAGE) & (D::CONFIG | D::PARAM) & D::ENCODER & D::INPUT));
     deprecated(ConfigMapper(PARAMETER_KEY_REQUEST_SYNC_FRAME,
                      "coding.request-sync", "value")
         .limitTo(D::PARAM & D::ENCODER)
@@ -1121,6 +1125,8 @@ status_t CCodecConfig::initialize(
     mParamUpdater->clear();
     mParamUpdater->supportWholeParam(
             C2_PARAMKEY_TEMPORAL_LAYERING, C2StreamTemporalLayeringTuning::CORE_INDEX);
+    mParamUpdater->supportWholeParam(
+            C2_PARAMKEY_QP_OFFSET_RECTS, C2StreamQpOffsetRects::CORE_INDEX);
     mParamUpdater->addParamDesc(mReflector, mParamDescs);
 
     // TEMP: add some standard fields even if not reflected
@@ -1867,6 +1873,31 @@ ReflectedParamUpdater::Dict CCodecConfig::getReflectedFormat(
                 params->setInt32((prefix + ".type").c_str(),
                                  HDR_DYNAMIC_METADATA_TYPE_SMPTE_2094_40);
                 params->setBuffer((prefix + ".data").c_str(), hdrDynamicInfo);
+            }
+        }
+    }
+
+    if (mDomain == (IS_VIDEO | IS_ENCODER)) {
+        AString qpOffsetRects;
+        if (params->findString(KEY_QP_OFFSET_RECTS, &qpOffsetRects)) {
+            std::vector<C2QpOffsetRectStruct> c2QpOffsetRects;
+            char* box = strtok(strdup(qpOffsetRects.c_str()), ";");
+            while (box != nullptr) {
+                int top, left, bot, right, qpOffset;
+                if (sscanf(box, "%d,%d-%d,%d=%d", &top, &left, &bot, &right, &qpOffset) == 5) {
+                    if (top >= 0 && left >= 0 && right >= left && bot >= top) {
+                        c2QpOffsetRects.push_back(C2QpOffsetRectStruct(
+                                right - left, bot - top, left, top, qpOffset));
+                    }
+                }
+                box = strtok(nullptr, ";");
+            }
+            if (c2QpOffsetRects.size() != 0) {
+                const std::unique_ptr<C2StreamQpOffsetRects::output> regions =
+                        C2StreamQpOffsetRects::output::AllocUnique(
+                                c2QpOffsetRects.size(), 0u, c2QpOffsetRects);
+                params->setBuffer(C2_PARAMKEY_QP_OFFSET_RECTS,
+                                  ABuffer::CreateAsCopy(regions.get(), regions->size()));
             }
         }
     }
--- media/libstagefright/include/media/stagefright/MediaCodecConstants.h
+++ media/libstagefright/include/media/stagefright/MediaCodecConstants.h
@@ -848,6 +848,8 @@ inline constexpr char KEY_VIDEO_QP_MIN[] = "video-qp-min";
 inline constexpr char KEY_VIDEO_QP_P_MAX[] = "video-qp-p-max";
 inline constexpr char KEY_VIDEO_QP_P_MIN[] = "video-qp-p-min";
 inline constexpr char KEY_WIDTH[] = "width";
+inline constexpr char KEY_QP_OFFSET_MAP[] = "qp-offset-map";
+inline constexpr char KEY_QP_OFFSET_RECTS[] = "qp-offset-rects";
 
 // from MediaCodec.java
 inline constexpr int32_t ERROR_INSUFFICIENT_OUTPUT_PROTECTION = 4;
