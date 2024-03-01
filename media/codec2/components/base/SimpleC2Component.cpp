5cc06d347b4d82a8bc04c06ff8f70188259ed097
Sungtak Lee <taklee@google.com>
2024-03-01 01:00:40 +0000

    WIP codec2 swcodec: stop process after stop()
    Change-Id: Id07f1ca7a653f122f7971f5ba69c5f7d15c3a0b1

--- media/codec2/components/base/SimpleC2Component.cpp
+++ media/codec2/components/base/SimpleC2Component.cpp
@@ -692,40 +692,53 @@ void SimpleC2Component::WorkHandler::onMessageReceived(const sp<AMessage> &msg) 
     switch (msg->what()) {
         case kWhatProcess: {
             if (mRunning) {
+                ALOGD("process begin");
                 if (thiz->processQueue()) {
+                    ALOGD("piggyback process queue");
                     (new AMessage(kWhatProcess, this))->post();
                 }
+                ALOGD("process end");
             } else {
                 ALOGV("Ignore process message as we're not running");
             }
             break;
         }
         case kWhatInit: {
+            ALOGD("init begin");
             int32_t err = thiz->onInit();
+            ALOGD("init end");
             Reply(msg, &err);
             [[fallthrough]];
         }
         case kWhatStart: {
+            ALOGD("start");
             mRunning = true;
             break;
         }
         case kWhatStop: {
+            ALOGD("stop begin");
             int32_t err = thiz->onStop();
             thiz->mOutputBlockPool.reset();
+            mRunning = false;
+            ALOGD("stop end");
             Reply(msg, &err);
             break;
         }
         case kWhatReset: {
+            ALOGD("reset begin");
             thiz->onReset();
             thiz->mOutputBlockPool.reset();
             mRunning = false;
+            ALOGD("reset end");
             Reply(msg);
             break;
         }
         case kWhatRelease: {
+            ALOGD("release begin");
             thiz->onRelease();
             thiz->mOutputBlockPool.reset();
             mRunning = false;
+            ALOGD("release end");
             Reply(msg);
             break;
         }
