02a01445030d9d0eee12259ccb306f82d0daeca8
Songyue Han <songyueh@google.com>
2024-03-21 20:15:35 +0000

    Create libmedia_codeclist_parser lib.
    Test: build
    Bug: b/306023029
    Change-Id: Ia7867fa8390770166ffd7104c084091470661ab5

---
+++ media/libmedia/include/media/MediaCodecInfoParserUtils.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MEDIA_CODEC_INFO_PARSER_UTILS_H_
+
+#define MEDIA_CODEC_INFO_PARSER_UTILS_H_
+
+#include <algorithm>
+#include <cmath>
+#include <optional>
+#include <vector>
+
+#include <media/stagefright/foundation/AUtils.h>
+
+namespace android {
+
+}  // namespace android
+
+#endif  // MEDIA_CODEC_INFO_UTILS_H_
